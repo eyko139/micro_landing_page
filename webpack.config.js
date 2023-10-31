3dfec51d7cf830f4aa5880d1acb3a8292d7c2676
Lukas Kühne <lukas.kuehne@prodyna.com>
2023-10-31 12:12:49 +0100

    namechange

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "talk",
-    projectName: "app1",
+    projectName: "nav",
     webpackConfigEnv,
     argv,
   });
